35a6200f45461c7e6db176406be775a882a516b1
Stefan Daschek <stefan@daschek.net>
2018-03-04 21:27:31 +0100

    Use frozen string literals

--- lib/cologne_phonetics.rb
+++ lib/cologne_phonetics.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'cologne_phonetics/rules'
 require 'cologne_phonetics/version'
 
--- lib/cologne_phonetics/rules.rb
+++ lib/cologne_phonetics/rules.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ColognePhonetics
   module Rules
     def self.define(&block)
